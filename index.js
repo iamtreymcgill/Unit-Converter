3d0a507e645e25457f1b3dbed5368f304ef59122
iamtreymcgill <iamtreymcgill@gmail.com>
2025-09-07 12:25:08 -0500

    Change to HTML and JS
    Got the Javascript working but looking to see if I can refactor it

--- index.js
+++ index.js
@@ -4,8 +4,33 @@
 1 kilogram = 2.204 pound
 */
 const convertBtn = document.getElementById("convert-btn")
+const input = document.getElementById("input")
+const length = document.getElementById("length")
+const volume = document.getElementById("volume")
+const mass = document.getElementById("mass")
 
-console.log(convertBtn)
-// convertBtn.addEventListener("click", function(){
-//     alert("Button, Clicked!")
-// })
+convertBtn.addEventListener("click", function(){
+        lengthConverter()
+        volumeConverter()
+        massConverter()
+})
+
+function lengthConverter (){
+    let meter = Number(input.value) / 3.28084
+    let feet = Number(input.value) * 3.28084
+    length.textContent = `${input.value} meters = ${feet.toFixed(3)} feet | ${input.value} feet = ${meter.toFixed(3)} meters`
+    // console.log(`${feet.toFixed(3)} ft ${meter.toFixed(3)} m`)
+}
+function volumeConverter (){
+    let liter = Number(input.value) / 0.264172
+    let gallon = Number(input.value) * 0.264172
+    volume.textContent = `${input.value} liters = ${gallon.toFixed(3)} gallons | ${input.value} gallons = ${liter.toFixed(3)} liters`
+    // console.log(`${liter.toFixed(3)} liters ${gallon.toFixed(3)} gallons`)
+}
+function massConverter (){
+    let kg = Number(input.value) / 2.20462
+    let lb = Number(input.value) * 2.20462
+    mass.textContent = `${input.value} kilos = ${lb.toFixed(3)} pounds | ${input.value} pounds = ${kg.toFixed(3)} kilos`
+    // console.log(`${kg.toFixed(3)} kilos ${lb.toFixed(3)} pounds`)
+}
+
